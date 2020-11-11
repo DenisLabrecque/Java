11a0024e5038acc392d465e3233aeea9e1482adf
DenisLabrecque <denis.g.labrecque@gmail.com>
2020-11-11 12:42:45 -0600

    added generic function calls for compilation

---
+++ PaperAssembly.java
@@ -0,0 +1,18 @@
+public class PaperAssembly {
+    public PaperAssembly(int i) {
+    }
+
+    public void activate() {
+    }
+
+    public boolean isActive() {
+        return true;
+    }
+
+    public String getValue() {
+        return "full";
+    }
+
+    public void deactivate() {
+    }
+}
