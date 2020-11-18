046df1abd2bbb0de2d947bba1888593c7974ff47
carn <joellikesgiraffes@gmail.com>
2020-11-18 04:36:40 -0600

    Added variables and functions

--- PrintAssembly.java
+++ PrintAssembly.java
@@ -1,51 +1,126 @@
 public class PrintAssembly extends AssemblyUnit implements ISimAssembly {
 
-    /**
-     * Constructor.
-     * @param laserPrinter Reference back to the printer for sending messages, warnings, and exceptions.
-     */
-    public PrintAssembly(LaserPrinter laserPrinter) {
-        super();
-    }
+	private final int MIRROR_RPM = 200;
+	private final int MIRROR_SPINUP = 50;
+	private final int MIRROR_SPINDOWN = 25;
+	private final int MAX_DRUM_LIFE = 500;
+	private final int DRUM_LIFE_WARNING = 100;
 
-    @Override
-    public void activate() throws AssemblyException {
+	private int rotationalSpeed;
+	private boolean coronaChargeStatus;
+	private boolean dischargeLampStatus;
+	private int sheetsPrinted;
 
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param laserPrinter Reference back to the printer for sending messages,
+	 *                     warnings, and exceptions.
+	 */
+	public PrintAssembly(LaserPrinter laserPrinter) {
+		super();
+		coronaChargeStatus = false;
+		dischargeLampStatus = false;
+		sheetsPrinted = 0;
+	}
 
-    @Override
-    public void deactivate() throws AssemblyException {
+	// Activates the PrintAssembly
+	@Override
+	public void activate() throws AssemblyException {
+		spinUpLaserMirror();
+		chargeCoronaWire();
+		turnOnDischargeLamp();
+	}
 
-    }
+	// Deactivates the PrintAssembly
+	@Override
+	public void deactivate() throws AssemblyException {
+		spinDownLaserMirror();
+		dischargeCoronaWire();
+		turnOffDischargeLamp();
+	}
 
-    @Override
-    public void setValue(int newValue) {
+	// Spins up the laser mirror
+	public void spinUpLaserMirror() {
+		while (rotationalSpeed < MIRROR_RPM) {
+			rotationalSpeed += MIRROR_SPINUP;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 
-    }
+	// Spins down the laser mirror
+	public void spinDownLaserMirror() {
+		while (rotationalSpeed > 0) {
+			rotationalSpeed -= MIRROR_SPINDOWN;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 
-    @Override
-    public int getValue() {
-        return 0;
-    }
+	// Charges the corona wire
+	public void chargeCoronaWire() {
+		coronaChargeStatus = true;
+	}
 
-    /**
-     * Makes the drum brand new (no longer used).
-     */
-    public void replaceDrum() {
-    }
+	// Discharges the corona wire
+	public void dischargeCoronaWire() {
+		coronaChargeStatus = false;
+	}
 
-    /**
-     * @return Current exception object (if an exception has occurred).
-     */
-    public AssemblyException exception() {
-        return null;
-    }
+	// Turns on the discharge lamp
+	public void turnOnDischargeLamp() {
+		dischargeLampStatus = true;
+	}
 
-    public boolean drumIsActive() {
-        return activated; // TODO
-    }
+	// Turns off the discharge lamp
+	public void turnOffDischargeLamp() {
+		dischargeLampStatus = false;
+	}
 
-    public boolean isWarning() {
-        return false; // TODO
-    }
+	// Increases the sheet counter each time the drum life is decreased by a new
+	// page being printed
+	public void updateDrumLife() {
+		sheetsPrinted++;
+	}
+
+	// Resets the sheet counter when the drum is replaced
+	public void replaceDrum() {
+		sheetsPrinted = 0;
+	}
+
+	/**
+	 * @return Current exception object (if an exception has occurred).
+	 */
+	public AssemblyException exception() {
+		return null;
+	}
+
+	// Status of PrintAssembly
+	public boolean drumIsActive() {
+		return activated;
+	}
+
+	// Return true if PrintAssembly has low drum life
+	public boolean isWarning() {
+		return MAX_DRUM_LIFE - sheetsPrinted <= DRUM_LIFE_WARNING;
+	}
+
+	@Override
+	public void setValue(int newValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- PrinterQueue.java
+++ PrinterQueue.java
@@ -1,43 +1,66 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class PrinterQueue implements ISimAssembly {
 
-    /**
-     * Constructor.
-     * @param laserPrinter Reference back to the printer for sending messages, warnings, and exceptions.
-     */
-    public PrinterQueue(LaserPrinter laserPrinter) {
-    }
+	private List<Document> printerQueue;
 
-    /**
-     * @param document Document to add to the print queue.
-     */
-    public void add(Document document) {
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param laserPrinter Reference back to the printer for sending messages,
+	 *                     warnings, and exceptions.
+	 */
+	public PrinterQueue(LaserPrinter laserPrinter) {
+		printerQueue = new LinkedList<Document>();
+	}
 
-    /**
-     * @param document Document to remove from the print queue.
-     */
-    public void remove(Document document) {
-    }
+	// Adds a document to the queue
+	public void add(Document document) {
+		printerQueue.add(document);
+	}
 
-    /**
-     * @return Current exception object (if an exception has occurred).
-     */
-    public AssemblyException exception() {
-        return null;
-    }
+	// Returns the queue list
+	public List<Document> returnList() {
+		return printerQueue;
+	}
 
-    @Override
-    public void setValue(int newValue) {
+	// Returns the next item in queue
+	public Document nextQueue() {
+		return printerQueue.get(0);
+	}
 
-    }
+	public void clearQueue() {
+		printerQueue.clear();
+	}
 
-    @Override
-    public int getValue() {
-        return 0;
-    }
+	// Removes a document from queue
+	public void remove(Document document) {
 
-    @Override
-    public void setError(int newValue) {
+	}
 
-    }
+	/**
+	 * @return Current exception object (if an exception has occurred).
+	 */
+	public AssemblyException exception() {
+		return null;
+	}
+
+	@Override
+	public void setValue(int newValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setError(int newValue) {
+		// TODO Auto-generated method stub
+
+	}
 }
